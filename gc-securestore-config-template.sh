7c7d35cc090f452d7249a6da48b6cae003e42287
apccc <abraham@apc.cc>
2017-03-10 14:06:42 -0800

    Update gc-securestore-config-template.sh

--- gc-securestore-config-template.sh
+++ gc-securestore-config-template.sh
@@ -4,5 +4,10 @@
 #space separated list of source buckets to backup
 #(local computer instance service account will need at least read only/viewer permission to project/buckets)
 SOURCE_BUCKETS=""
+
+#a unique storage prefix (short and unique)
+STORE_PREFIX=""
+
+#default storage info
 STORAGE_CLASS="DURABLE_REDUCED_AVAILABILITY"
 STORAGE_LOCATION="US"
