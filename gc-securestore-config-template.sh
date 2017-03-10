4344ff207ba99cf700d0745e3506a148535e88de
apccc <abraham@apc.cc>
2017-03-10 13:38:44 -0800

    Update gc-securestore-config-template.sh

--- gc-securestore-config-template.sh
+++ gc-securestore-config-template.sh
@@ -4,4 +4,3 @@
 #space separated list of source buckets to backup
 #(local computer instance service account will need at least read only/viewer permission to project/buckets)
 SOURCE_BUCKETS=""
-exit 0
