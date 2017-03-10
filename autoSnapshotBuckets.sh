42c3b59a2c832f3fb5b45d7282b5bc1ebb90588c
apccc <abraham@apc.cc>
2017-03-10 14:12:32 -0800

    Update autoSnapshotBuckets.sh

--- autoSnapshotBuckets.sh
+++ autoSnapshotBuckets.sh
@@ -16,5 +16,11 @@ for SOURCE_BUCKET in `echo "$SOURCE_BUCKETS"`;do
   echo "SOURCE BUCKET: $SOURCE_BUCKET"
   MM=$(date +%m)
   W=$(($((7+$(date +%-d)))/7))
+
+  MONTH_TARGET_BUCKET=`echo "${STORE_PREFIX}-M${MM}-${SOURCE_BUCKET}" | cut -c 1-62`
+  WEEK_TARGET_BUCKET=`echo "${STORE_PREFIX}-W${W}-${SOURCE_BUCKET}" | cut -c 1-62`
+
+  echo "MONTH TARGET BUCKET: $MONTH_TARGET_BUCKET"
+  echo "WEEK TARGET BUCKET: $WEEK_TARGET_BUCKET"
 done
 exit 0
