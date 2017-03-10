819ad07441aa5d4535985de2a7ff710dc9cc4101
apccc <abraham@apc.cc>
2017-03-10 12:54:13 -0800

    Update gc-securestore-config-template.sh

--- gc-securestore-config-template.sh
+++ gc-securestore-config-template.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #space separated list of source buckets to backup
+#(local gsutil service account will need at least read only/viewer permission to project/buckets)
 SOURCE_BUCKETS=""
 exit 0
