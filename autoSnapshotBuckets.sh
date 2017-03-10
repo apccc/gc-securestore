9423b73663d3f0ba14e94c0e87f184d3343060db
apccc <abraham@apc.cc>
2017-03-10 11:50:54 -0800

    Create autoSnapshotBuckets.sh

---
+++ autoSnapshotBuckets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Auto Snapshotting Buckets into Secure Store!"
+CURRENT_DIR=`dirname "$0"`
+if [ ! -f "~/gc-securestore-config.sh" ];then
+  touch "~/gc-securestore-config.sh"
+  chmod 600 "~/gc-securestore-config.sh"
+  cp "$CURRENT_DIR/gc-securestore-config-template.sh" "~/gc-securestore-config.sh"
+  echo "Please setup config file! ~/gc-securestore-config.sh"
+  exit
+fi
+exit 0
