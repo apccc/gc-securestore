fd378023b2ba31ce2e49c83dc1fe518ff6f61c9b
apccc <abraham@apc.cc>
2017-03-10 14:01:18 -0800

    Update getProjectId.sh

--- getProjectId.sh
+++ getProjectId.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#pull the project id
 curl http://metadata.google.internal/computeMetadata/v1/project/project-id -H "Metadata-Flavor: Google" 2>&1 | egrep -oe '[a-zA-Z0-9_.-]+$' | tail -n 1
 exit 0
